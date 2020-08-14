b1a6983ec4972fed13f7a1f2bb4e2b0da75fab16
charles_window <charles_window@DESKTOP-DGBVTC6>
2020-08-14 17:29:39 +0900

    Submission successful

--- ALGO_branch_sums/src/Program_branch_sums.java
+++ ALGO_branch_sums/src/Program_branch_sums.java
@@ -1,35 +1,48 @@
 import java.util.*;
 
 class Program {
-	// This is the class of the input root. Do not edit it.
-	public static class BinaryTree {
-		int value;
-		BinaryTree left;
-		BinaryTree right;
+  // This is the class of the input root. Do not edit it.
+  public static class BinaryTree {
+    int value;
+    BinaryTree left;
+    BinaryTree right;
 
-		BinaryTree(int value) {
-			this.value = value;
-			this.left = null;
-			this.right = null;
-		}
+    BinaryTree(int value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  public static List<Integer> branchSums(BinaryTree root) {
+    // Write your code here.
+				// Write your code here.
+		List<Integer> sums = new ArrayList<Integer>();
+		listBranchSums(root,0, sums);
+		return sums;
 	}
 
-	public static List<Integer> branchSums(BinaryTree root) {
-		// Write your code here.
-		if(root.left==null) {
-			return 
+
+	private static void listBranchSums(BinaryTree root,int numSum, List<Integer> sums) {
+		// TODO Auto-generated method stub
+		if(root ==null)
+			return;
+		
+		numSum += root.value;
+		if(root.left==null&&root.right==null) {
+			sums.add(numSum); 
+			return;
 		}
-		root.value;
-		branchSums(root.left);
-		branchSums(root.right);
-
-		return new ArrayList<Integer>();
+		
+		
+		listBranchSums(root.left,numSum,sums);
+		listBranchSums(root.right,numSum,sums);
 	}
-
+}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		BinaryTree first 
 	}
 
 }
